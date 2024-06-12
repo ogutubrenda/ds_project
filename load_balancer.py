273dc9a9a705177e7855cede19368b8aa2b44a31
ogutubrenda <brenda.ogutu@strathmore.edu>
2024-06-12 11:35:11 +0300

    Initial commit

--- load_balancer.py
+++ load_balancer.py
@@ -1,58 +1,54 @@
 from flask import Flask, request, jsonify
-import consistent_hash
+from consistent_hash import ConsistentHash
+import docker
 
 app = Flask(__name__)
+ch = ConsistentHash()
+client = docker.from_env()
 
-servers = []
-consistent_hash_map = consistent_hash.ConsistentHashMap(num_slots=512, num_servers=3, num_virtual_servers=9)
-
-for i in range(3):
-    server_id = f"Server {i+1}"
-    servers.append(server_id)
-    consistent_hash_map.add_server(server_id)
+N = 3  # Initial number of servers
+servers = [f'Server{i+1}' for i in range(N)]
+for server in servers:
+    ch.add_node(server)
 
 @app.route('/rep', methods=['GET'])
 def get_replicas():
-    return jsonify({"replicas": servers})
+    return jsonify({"N": len(ch.nodes)//ch.num_virtual_nodes, "replicas": list(ch.nodes.values())})
 
 @app.route('/add', methods=['POST'])
-def add_servers():
-    data = request.get_json()
-    n = data.get('n')
+def add_replicas():
+    data = request.json
+    num_instances = data['n']
     hostnames = data.get('hostnames', [])
-
-    if len(hostnames) > n:
-        return jsonify({"error": "Length of hostname list is more than newly added instances"}), 400
-
-    for i in range(n):
-        server_id = hostnames[i] if i < len(hostnames) else f"Server {len(servers) + 1}"
-        servers.append(server_id)
-        consistent_hash_map.add_server(server_id)
-
-    return jsonify({"replicas": servers})
+    if len(hostnames) > num_instances:
+        return jsonify({"message": "Error: Length of hostname list is more than newly added instances", "status": "failure"}), 400
+    for i in range(num_instances):
+        hostname = hostnames[i] if i < len(hostnames) else f"Server{len(ch.nodes)//ch.num_virtual_nodes + 1}"
+        ch.add_node(hostname)
+        # Code to spawn a new Docker container can be added here
+    return jsonify({"message": {"N": len(ch.nodes)//ch.num_virtual_nodes, "replicas": list(ch.nodes.values())}, "status": "successful"})
 
 @app.route('/rm', methods=['DELETE'])
-def remove_servers():
-    data = request.get_json()
-    n = data.get('n')
+def remove_replicas():
+    data = request.json
+    num_instances = data['n']
     hostnames = data.get('hostnames', [])
-
-    if len(hostnames) > n:
-        return jsonify({"error": "Length of hostname list is more than removable instances"}), 400
-
-    for hostname in hostnames:
-        if hostname in servers:
-            servers.remove(hostname)
-            consistent_hash_map.remove_server(hostname)
-
-    return jsonify({"replicas": servers})
+    if len(hostnames) > num_instances:
+        return jsonify({"message": "Error: Length of hostname list is more than removable instances", "status": "failure"}), 400
+    for i in range(num_instances):
+        hostname = hostnames[i] if i < len(hostnames) else None
+        if hostname:
+            ch.remove_node(hostname)
+            # Code to remove Docker container can be added here
+    return jsonify({"message": {"N": len(ch.nodes)//ch.num_virtual_nodes, "replicas": list(ch.nodes.values())}, "status": "successful"})
 
 @app.route('/<path>', methods=['GET'])
 def route_request(path):
-    request_id = request.environ.get('HTTP_X_REQUEST_ID', '123456')
-    server_id = consistent_hash_map.map_request(request_id)
-    # Implement logic to forward the request to the server instance
-    return f"Request for /{path} routed to {server_id}"
+    server = ch.get_node(path)
+    if server:
+        # Code to forward the request to the chosen server
+        return jsonify({"message": f"Routed to {server}", "status": "successful"}), 200
+    return jsonify({"message": f"Error: '{path}' endpoint does not exist in server replicas", "status": "failure"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
--- server.py
+++ server.py
@@ -1,16 +1,18 @@
-#Contains the code for the simple web server with the /home and /heartbeat endpoints. (TASK  1)
-from flask import Flask
+from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
+server_id = os.getenv('SERVER_ID', '1')
+
 @app.route('/home', methods=['GET'])
 def home():
-    # Replace 'WORKING SERVER' with a unique identifier
-    return 'Hello from WORKING SERVER'
+    return jsonify({"message": f"Hello from Server: {server_id}", "status": "successful"}), 200
 
 @app.route('/heartbeat', methods=['GET'])
 def heartbeat():
     return '', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
